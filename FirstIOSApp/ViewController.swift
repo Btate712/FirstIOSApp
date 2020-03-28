79d5acf8a70d27c61cc77a63a362a69f91816613
btate712 <btate712@gmail.com>
2020-03-28 18:30:01 -0500

    created single view app with xcode

---
+++ FirstIOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstIOSApp
+//
+//  Created by Robert Tate on 3/28/20.
+//  Copyright © 2020 Robert Tate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
